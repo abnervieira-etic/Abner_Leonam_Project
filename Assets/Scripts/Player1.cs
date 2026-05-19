3257397eb643bd88fc72ea60e41402e0fcdfcd45
Abner Vieira <abner_vieira@eticalgarve.com>
2026-05-19 12:16:20 +0100

    Initial commit

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    float speed = 2f;
+    Rigidbody2D rb1; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
