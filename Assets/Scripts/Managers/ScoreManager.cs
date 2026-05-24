e5ec6102d8dd3abf2d04e06abe347686010fde03
queisso1 <oleonam7@gmail.com>
2026-05-24 18:16:13 +0100

    reload da scene

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
-
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 public class ScoreManager : MonoBehaviour
 {
     public int leftGoalScore = 0;
@@ -9,27 +10,32 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager SMinstance;
     [SerializeField] TMP_Text leftGoalTxt;
     [SerializeField] TMP_Text rightGoalTxt;
-    private bool doubleGoal;
+ 
     void Start()
     {
         if (SMinstance == null)
             SMinstance = this;
 
     }
-
-    void Update()
-    {
-        
-    }
-
        public void AddScore1()
     {
         leftGoalScore++;
-        leftGoalTxt.text = "" + leftGoalScore.ToString();            
+        leftGoalTxt.text = "" + leftGoalScore.ToString();  
+        if(leftGoalScore == 5)
+        {            
+            Debug.Log("Player 1 Wins!");
+            SceneManager.LoadScene("StartMenu");
+
+        }
     }
        public void AddScore2()
     {
         rightGoalScore++;
         rightGoalTxt.text = "" + rightGoalScore.ToString();   
+        if(rightGoalScore == 5)
+        {
+            Debug.Log("Player 2 Wins!");
+            SceneManager.LoadScene("StartMenu");
+        }
     }
 }
